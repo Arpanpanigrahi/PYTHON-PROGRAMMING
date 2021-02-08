9bf9eea38663f7215bd1958e83a92de4ef2c3a58
Arpanpanigrahi <46858538+Arpanpanigrahi@users.noreply.github.com>
2021-02-08 13:02:38 +0530

    Add files via upload

---
+++ Assignment 01 25th may/average.py
@@ -0,0 +1,9 @@
+def cal_average(num):
+    sum_num = 0
+    for t in num:
+        sum_num = sum_num + t           
+
+    avg = sum_num / len(num)
+    return avg
+
+print("The average is", cal_average([18,25,3,41,5]))
---
+++ Assignment 01 25th may/facevalue.py
@@ -0,0 +1,7 @@
+face_value = int(input("please enter the face value: "))
+
+sum_ = 7
+if 0 < face_value <= 6:
+    print("The opposition value of the face value is:", sum_-face_value)
+else:
+    print("Please enter a value between 1 to 6(including both)")
